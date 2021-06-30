b287f0f6c1e84244fc1ceeaccb8016a442d198b4
TabsOverSpaces4 <guptaharsh.2401@gmail.com>
2021-06-30 22:45:14 +0530

    asset management

--- lib/Pages/LSOptions.dart
+++ lib/Pages/LSOptions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:planify/animation/try_fade.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -8,8 +9,107 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.blueGrey[900],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                FadeAnimation(
+                  1,
+                  Container(
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.transparent,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 100,
+                          backgroundImage: AssetImage(
+                            "lib/assets/images/logo.png",
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                FadeAnimation(
+                  1,
+                  Text(
+                    "CRYPTRACK",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                FadeAnimation(
+                  1,
+                  Container(
+                    height: 50,
+                    margin: EdgeInsets.symmetric(horizontal: 60),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.pinkAccent[700],
+                    ),
+                    child: Center(
+                        child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                FadeAnimation(
+                  1,
+                  Container(
+                    height: 50,
+                    margin: EdgeInsets.symmetric(horizontal: 60),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.pinkAccent[700],
+                    ),
+                    child: Center(
+                        child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signin');
+                      },
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
